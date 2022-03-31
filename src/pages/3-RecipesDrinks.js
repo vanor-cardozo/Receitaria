0a77b964fb5b2c0f270529eabdb2676258d2042f
Vanor-Cardozo <vanor.fc@gmail.com>
2022-03-31 19:32:06 -0300

    feat: :zap: Implementa o MAP para comidas e bebidas

--- src/pages/3-RecipesDrinks.js
+++ src/pages/3-RecipesDrinks.js
@@ -1,10 +1,12 @@
 import React, { useContext, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import RecipesContext from '../context/RecipesContext';
 import fetchDrinks from '../utils/fetchDrinks';
 
 function RecipesDrinks() {
-  const { setDrinksApi, API } = useContext(RecipesContext);
+  const { setDrinksApi, API, drinksApi } = useContext(RecipesContext);
+  const history = useHistory();
 
   useEffect(() => {
     if (API) {
@@ -15,10 +17,32 @@ function RecipesDrinks() {
       api();
     }
   }, [API, setDrinksApi]);
+
+  if (drinksApi && drinksApi.drinks.length === 1) {
+    const id = drinksApi.drinks[0].idDrink;
+    history.push(`/drinks/${id}`);
+  }
+  const MAX_DRINKS = 12;
+  const drinks = drinksApi && drinksApi.drinks.filter((_, i) => i < MAX_DRINKS);
+
   return (
     <>
       <Header title="Drinks" />
       <h1>Tela principal de receitas de bebidas.</h1>
+      {
+        drinksApi && drinksApi.drinks.length > 1
+          ? drinks.map((drink, index) => (
+            <div key={ drink.idDrink } data-testid={ `${index}-recipe-card` }>
+              <h4 data-testid={ `${index}-card-name` }>{ drink.strDrink }</h4>
+              <img
+                src={ drink.strDrinkThumb }
+                alt={ drink.strDrink }
+                width="20%"
+                data-testid={ `${index}-card-img` }
+              />
+            </div>))
+          : ''
+      }
     </>
   );
 }
