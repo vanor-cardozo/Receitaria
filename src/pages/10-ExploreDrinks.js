cfee7f551c668b646b66233c7e8c722002ef6f03
Gilson Nogueira <engnogueira@hotmail.com>
2022-04-01 17:14:35 -0300

    Commit com comentário para subir novo PR
    Signed-off-by: Gilson Nogueira <engnogueira@hotmail.com>

--- src/pages/10-ExploreDrinks.js
+++ src/pages/10-ExploreDrinks.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Footer from '../components/Footer';
 import HeaderWithoutSearchButton from '../components/HeaderWithoutSearchButton';
-import OptionsDrinks from '../components/OptionsDrinks';
+import OptionsDrinks from '../components/OptionsDrinks'; // Commit novo PR
 
 function ExploreDrinks() {
   return (
     <>
       <HeaderWithoutSearchButton title="Explore Drinks" />
       <OptionsDrinks />
-      {/* <h1>Explore Drinks</h1> */}
+      {/* <h1>Explore Drinks</h1> */} 
       <Footer />
     </>
   );
