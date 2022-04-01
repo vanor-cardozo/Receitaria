5d16bab59bfc5c4944fd6677f3e6c300f9458855
julialanapatto <julialanapatto@gmail.com>
2022-04-01 15:13:28 -0300

    main-group-2-feat-explore

--- src/pages/8-Explore.js
+++ src/pages/8-Explore.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import HeaderWithoutSearchButton from '../components/HeaderWithoutSearchButton';
 
 function Explore() {
+  const history = useHistory();
   return (
     <>
       <HeaderWithoutSearchButton title="Explore" />
       <h1>Explore</h1>
+      <div>
+        <button
+          type="button"
+          data-testid="explore-foods"
+          onClick={ () => history.push('/explore/foods') }
+        >
+          Explore Foods
+        </button>
+        <button
+          type="button"
+          data-testid="explore-drinks"
+          onClick={ () => history.push('/explore/drinks') }
+        >
+          Explore Drinks
+        </button>
+      </div>
       <Footer />
     </>
   );
