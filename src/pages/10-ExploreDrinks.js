009c40119b57af6a1ef8c744093b83f5759fa868
Gilson Nogueira <engnogueira@hotmail.com>
2022-04-01 21:04:20 -0300

    Fix explore foods and drinks
    Signed-off-by: Gilson Nogueira <engnogueira@hotmail.com>

--- src/pages/10-ExploreDrinks.js
+++ src/pages/10-ExploreDrinks.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Footer from '../components/Footer';
 import HeaderWithoutSearchButton from '../components/HeaderWithoutSearchButton';
+import OptionsDrinks from '../components/OptionsDrinks'; // Commit novo PR
 
 function ExploreDrinks() {
   return (
     <>
       <HeaderWithoutSearchButton title="Explore Drinks" />
-      <h1>Explore Drinks</h1>
+      <OptionsDrinks />
+      {/* <h1>Explore Drinks</h1> */}
       <Footer />
     </>
   );
