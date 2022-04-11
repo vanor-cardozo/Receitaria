e39c1d8e3d1e7fa1b37eff8d7b1822d5699ebc85
Leandro de Oliveira <19leandro3quietmsl@gmail.com.br>
2022-04-11 14:49:43 -0300

    Explore Foods Nationalities screen

--- src/context/RecipesProvider.js
+++ src/context/RecipesProvider.js
@@ -1,25 +1,14 @@
 import React, { useState } from 'react';
-// import './App.css';
 import PropTypes from 'prop-types';
 import RecipesContext from './RecipesContext';
 
 function RecipesProvider({ children }) {
-  const [API, setAPI] = useState();// const [data, setData] = useState([]);
+  const [API, setAPI] = useState();
   const [foodsApi, setFoodsApi] = useState({ meals: [] });
   const [drinksApi, setDrinksApi] = useState({ drinks: [] });
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [redirect, setRedirect] = useState(false);
-
-  // useEffect(() => {
-  //   if (API) {
-  //     const api = async () => {
-  //       const result = await fetchFoods(API);
-  //       setFoodsApi(result);
-  //     };
-  //     api();
-  //   }
-  // }, [API]);
 
   const values = {
     setAPI,
