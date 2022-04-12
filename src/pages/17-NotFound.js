86724657e6fa429ba5d28c86d87870d652d1c0a4
Gilson Nogueira <engnogueira@hotmail.com>
2022-04-12 08:44:43 -0300

    Desenvolvimento da tela 'Not Found' - inclusão do comentário para execução do stash

--- src/pages/17-NotFound.js
+++ src/pages/17-NotFound.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function NotFound() {
   return (
-    <div>Not Found</div>
+    <div>Not Found</div> // Alteração para gerar o stash para criar nova branch
   );
 }
